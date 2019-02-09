ac7e6453c90427133e2139428aa754428da5a2fd
Josh Bohde <josh@joshbohde.com>
2019-02-09 12:31:21 -0600

    Add a simulation test.

---
+++ sim_test.go
@@ -0,0 +1,103 @@
+package congestion
+
+import (
+	"context"
+	"errors"
+	"flag"
+	"math/rand"
+	"sync"
+	"sync/atomic"
+	"testing"
+	"time"
+)
+
+var sim = flag.Bool("sim", false, "run simulation test")
+
+func msToWait(perSec int64) time.Duration {
+	ms := rand.ExpFloat64() / (float64(perSec) / 1000)
+	return time.Duration(ms * float64(time.Millisecond))
+}
+
+type Capped struct {
+	mu  sync.Mutex
+	cur int64
+	cap int64
+}
+
+func (s *Capped) Lock() error {
+	s.mu.Lock()
+	if s.cur >= s.cap {
+		s.mu.Unlock()
+		return errors.New("too many threads")
+	}
+
+	s.cur++
+	s.mu.Unlock()
+
+	return nil
+}
+
+func (s *Capped) Unlock() {
+	s.mu.Lock()
+	s.cur--
+	s.mu.Unlock()
+}
+
+// Simulate 2 concurrent process with 1000 reqs/second each, fighting
+// for a process that can process 10 concurrent at 100 reqs/second
+// This should converge to each limiter getting 5, with a success rate of 50% on each
+func TestConcurrentSimulation(t *testing.T) {
+	if !(*sim) {
+		t.Log("Skipping sim since -sim not passed")
+		t.Skip()
+	}
+
+	wg := sync.WaitGroup{}
+
+	c := Capped{cap: 10}
+
+	for i := 0; i < 2; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			inner := sync.WaitGroup{}
+
+			limiter := New(Config{
+				Capacity: 100,
+				MaxLimit: 20,
+			})
+
+			success := int64(0)
+
+			for i := 0; i < 1000; i++ {
+				time.Sleep(msToWait(1000))
+				inner.Add(1)
+
+				go func() {
+					defer inner.Done()
+					err := limiter.Acquire(context.Background(), 0)
+					if err != nil {
+						return
+					}
+					defer limiter.Release()
+
+					err = c.Lock()
+					if err != nil {
+						limiter.Backoff()
+						return
+					}
+					defer c.Unlock()
+					time.Sleep(msToWait(100))
+					atomic.AddInt64(&success, 1)
+
+				}()
+			}
+
+			t.Logf("limit=%d, success=%f", limiter.limit, (float64(success) / 1000))
+
+		}()
+	}
+
+	wg.Wait()
+
+}
