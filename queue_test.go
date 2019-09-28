722c501639c271b39dbbef3a7dad65e978980259
Josh Bohde <josh@joshbohde.com>
2019-09-28 13:26:58 -0500

    Failing test case.

--- queue_test.go
+++ queue_test.go
@@ -1,6 +1,7 @@
 package congestion
 
 import (
+	"fmt"
 	"testing"
 )
 
@@ -12,6 +13,7 @@ func TestPriority(t *testing.T) {
 		{[]int{0, 1}, 1},
 		{[]int{1, 0}, 1},
 		{[]int{0, 2, 1}, 2},
+		{[]int{0, 2, 1, 3, 7, 8}, 8},
 	}
 
 	for _, tc := range cases {
@@ -20,6 +22,7 @@ func TestPriority(t *testing.T) {
 			r := rendezvouz{priority: p}
 			q.Push(&r)
 		}
+
 		actual := q.Pop().priority
 		if actual != tc.Expected {
 			t.Errorf("Priority %v = %d, expected %d", tc.Priorities, actual, tc.Expected)
@@ -47,23 +50,36 @@ func TestRemove(t *testing.T) {
 }
 
 func TestDropLast(t *testing.T) {
-	a := rendezvouz{priority: 0, errChan: make(chan error, 1)}
-	b := rendezvouz{priority: 1, errChan: make(chan error, 1)}
-	c := rendezvouz{priority: 2, errChan: make(chan error, 1)}
+	cases := []int{2, 3, 4, 5, 6, 7, 8}
 
-	q := newQueue(2)
+	for _, c := range cases {
+		c := c
+		t.Run(fmt.Sprintf("%d", c), func(t *testing.T) {
+			a := rendezvouz{priority: 0, errChan: make(chan error, 1)}
 
-	for _, r := range []*rendezvouz{&a, &b, &c} {
-		q.Push(r)
-	}
+			q := newQueue(c)
+			q.Push(&a)
 
-	if q.Len() != 2 {
-		t.Errorf("Got %d, expected %d", 2, q.Len())
-	}
+			for i := 0; i < c; i++ {
+				r := rendezvouz{priority: i + 1, errChan: make(chan error, 1)}
+				q.Push(&r)
+			}
 
-	dropped := <-a.errChan
-	if dropped != Dropped {
-		t.Errorf("Got %d, expected %d", dropped, Dropped)
+			if q.Len() != c {
+				t.Errorf("Got %d, expected %d", c, q.Len())
+			}
+
+			var dropped error
+
+			select {
+			case dropped = <-a.errChan:
+			default:
+			}
+
+			if dropped != Dropped {
+				t.Errorf("Got %d, expected %d", dropped, Dropped)
+			}
+		})
 	}
 
 }
@@ -74,6 +90,20 @@ func BenchmarkQueue(b *testing.B) {
 
 	for i := 0; i < b.N; i++ {
 		q.Push(&r)
+	}
+}
+
+func BenchmarkQueueFull(b *testing.B) {
+	q := newQueue(10)
+
+	for i := 0; i < 10; i++ {
+		r := &rendezvouz{}
+		q.Push(r)
+	}
+
+	for i := 0; i < b.N; i++ {
+		r := &rendezvouz{priority: i + 1}
+		q.Push(r)
 	}
 }
 
